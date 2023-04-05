29ea53404be257c0934d54ef84233ba90fb305d0
iampooran <pooransen50@gmail.com>
2023-04-05 16:23:09 +0530

    File and Folder Structuer Done with App.css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import { Brand, CTA, Navbar } from "./components";
+import { Blog, Features, Footer, Header, Possibility, WhatGPT3 } from "./container";
+import "./App.css"
+
 function App() {
   return (
-    <div>
-      App
+    <div className="App">
+      <div className="gradient__bg">
+        <Navbar/>
+        <Header/>
+      </div>
+      <Brand/>
+      <WhatGPT3/>
+      <Features/>
+      <Possibility/>
+      <CTA/>
+      <Blog/>
+      <Footer/>
     </div>
   );
 }
